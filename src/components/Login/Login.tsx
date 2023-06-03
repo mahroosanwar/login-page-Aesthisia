63f4407b06509cde2f7a82d0b8d3c6c04abaa317
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2023-06-03 06:59:37 +0530

    some minor changes

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -8,23 +8,23 @@ import Button from "../Button/Button";
 const Login: React.FC = () => {
   return (
     <>
-      <div className="bg-white_A700 flex flex-col font-poppins items-start justify-end mx-auto pt-5 px-5 w-full">
+      <div className="bg-white_A700 flex flex-col font-poppins items-start justify-end mx-auto pt-5 px-5 w-full mt-14 md:mb-4 md:mt-6">
         <div className="flex md:flex-col flex-row md:gap-10 items-center justify-between mt-[7px] mx-auto md:px-5 w-full">
           {/* Left Side */}
           <div className="h-[555px] relative w-full mr-52 md:mr-0">
             <div className="h-[555px] m-auto w-full">
               <Img
                 src="images/img_rectangle15.png"
-                className="h-[555px] m-auto object-cover rounded-[20px] w-full"
+                className="relative h-[555px] m-auto object-cover rounded-[20px] w-full"
                 alt="rectangleFifteen"
               />
               <Img
                 src="images/img_frame452.png"
-                // className="h-[22px] md:h-auto mr-[55px] object-cover w-[54%]"
+                className="h-[22px] md:h-auto mr-[55px] object-cover w-[54%]"
                 alt="frame452"
               />
               <Text
-                className="absolute bottom-[29%] left-[3%] text-white_A700_b2 tracking-[1.44px]"
+                className="bottom-[29%] left-[3%] text-white_A700_b2 tracking-[1.44px]"
                 as="h1"
                 variant="h1"
               >
@@ -42,22 +42,23 @@ const Login: React.FC = () => {
                   Infrastructure
                 </span>
                 <span className="md:text-[26px] sm:text-2xl text-gray_400 text-[28px] font-poppins text-left font-normal">
+                  {" "}
                   Management
                 </span>
               </Text>
             </div>
             <div className="absolute flex flex-row sm:gap-10 h-max inset-[0] items-start justify-center m-auto w-[93%]">
-              <div>
+              <div className="absolute left-0 bottom-0">
                 <Img
                   src="images/img_internet11.svg"
                   className="h-[15px] mt-0.5 w-[15px]"
                   alt="internetEleven"
                 />
-                <Text className="ml-0.5 w-auto" as="h5" variant="h5">
+                <Text className="ml-0.5 w-auto text-white" as="h5" variant="h5">
                   aesthisia.com
                 </Text>
               </div>
-              <div className="flex flex-1 flex-row gap-1.5 h-[15px] md:h-auto items-start justify-start mt-[485px] w-full">
+              <div className="flex flex-1 flex-row gap-1.5 h-[15px] md:h-auto items-start justify-end mt-[485px] w-full">
                 <Img
                   src="images/img_linkedin.svg"
                   className="h-[15px] w-[15px]"
@@ -77,7 +78,7 @@ const Login: React.FC = () => {
             </div>
           </div>
           {/* Right Side */}
-          <div className="flex md:flex-1 flex-col font-inter gap-7 items-center justify-start w-[33%] md:w-full">
+          <div className="flex md:flex-1 flex-col font-inter gap-7 items-center justify-start w-[33%] md:w-full mr-24 md:mr-0">
             {/* Above Part */}
             <div className="flex flex-col items-center justify-start w-[74%] md:w-full">
               <Img
@@ -146,7 +147,7 @@ const Login: React.FC = () => {
                 </Text>
               </a>
               <Button
-                className="cursor-pointer font-inter font-semibold h-[53px] mt-9 text-center text-lg text-white_A700 tracking-[-0.14px] w-[360px]"
+                className="cursor-pointer font-inter font-semibold h-[53px] mt-9 text-center text-lg text-white_A700 tracking-[-0.14px]  w-full"
                 shape="RoundedBorder10"
                 size="sm"
                 variant="OutlineBlack9006b"
@@ -165,10 +166,8 @@ const Login: React.FC = () => {
                   {" "}
                 </span>
                 <span className="text-teal_500 text-sm font-inter font-medium">
-                  Sign Up
-                </span>
-                <span className="text-blue_gray_700 text-base font-inter font-medium">
                   {" "}
+                  Sign Up
                 </span>
               </Text>
             </div>
